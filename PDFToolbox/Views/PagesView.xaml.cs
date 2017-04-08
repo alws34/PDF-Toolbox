c0f6566f906ae2f40564781a96934fa58db12abb
Cheeseman5 <dhuber04@gmail.com>
2017-04-07 19:02:51 -0500

    Drop events added to PagesView code-behind

--- PDFToolbox/Views/PagesView.xaml.cs
+++ PDFToolbox/Views/PagesView.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Shapes;
 
 using PDFToolbox.Common.ViewModels;
 using PDFToolbox.ViewModels;
+using PDFToolbox.IO;
 
 namespace PDFToolbox.Views
 {
@@ -45,6 +46,7 @@ namespace PDFToolbox.Views
         private void lbxPages_Drop(object sender, DragEventArgs e)
         {
             //TODO: Need to copy from MainWindow.xaml.cs.  Or do it right this time.
+            OnObjectDropped(sender, e);
         }
         private void lbxPages_DragEnter(object sender, DragEventArgs e)
         {
@@ -59,6 +61,50 @@ namespace PDFToolbox.Views
         private void lbxPages_PreviewMouseMove(object sender, MouseEventArgs e)
         {
             //TODO: Need to copy from MainWindow.xaml.cs.  Or do it right this time.
+        }
+
+        private void OnObjectDropped(object sender, DragEventArgs e)
+        {
+            HitTestResult hit = VisualTreeHelper.HitTest(sender as ListBox, e.GetPosition(sender as ListBox));
+
+            // DraggedItem is a pageDict -> rearrange
+            if (e.Data.GetDataPresent(typeof(ViewModels.PageViewModel)))
+            {
+
+                PageViewModel draggedPage = e.Data.GetData(typeof(ViewModels.PageViewModel)) as ViewModels.PageViewModel;
+                ListBoxItem lbxItemDropTarget = Toolbox.FindParent<ListBoxItem>(hit.VisualHit);
+                PageViewModel targetPage;
+                int sourceIndex = Document.GetPageIndex(draggedPage);
+                int targetIndex = Document.Pages.Count - 1;
+
+                // Move pageDict to last element if dropped on blank-space
+                if (lbxItemDropTarget != null)
+                {
+                    targetPage = lbxItemDropTarget.DataContext as PageViewModel;
+                    targetIndex = Document.GetPageIndex(targetPage);
+                }
+                Document.Pages.Move(sourceIndex, targetIndex);
+
+                return;
+            }
+
+            // Get any files dropped onto pageview
+            Models.Document[] dropFiles = FileIO.ExtractDocument(e.Data);
+
+            if(Document==null)
+            {
+                Document = new DocumentViewModel(new Models.Document());
+            }
+
+            // If any files dropped, load their pages
+            if (dropFiles != null && dropFiles.Length > 0)
+            {
+                for (int i = 0; i < dropFiles.Length; i++)
+                {
+                    Document.AddPages(dropFiles[i].pages.ToArray());
+                }
+                return;
+            }
         }
 
 
