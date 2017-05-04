454d14601dd04ca31d89849a1786d106c3cf46a8
Cheeseman5 <dhuber04@gmail.com>
2017-05-03 21:58:42 -0500

    Added UIHelper & ListBoxDropBehavior

---
+++ PDFToolbox/Behaviors/ListBoxDropBehavior.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Interactivity;
+using System.Windows.Controls;
+
+using PDFToolbox.Common;
+
+namespace PDFToolbox.Behaviors
+{
+    public class ListBoxDropBehavior : Behavior<ItemsControl>
+    {
+        private Type _droppableType;
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.AllowDrop = true;
+            AssociatedObject.DragEnter += new DragEventHandler(AssociatedObject_DragEnter);
+            AssociatedObject.DragOver += new DragEventHandler(AssociatedObject_DragOver);
+            AssociatedObject.DragLeave += new DragEventHandler(AssociatedObject_DragLeave);
+            AssociatedObject.Drop += new DragEventHandler(AssociatedObject_Drop);
+        }
+
+        void AssociatedObject_Drop(object sender, DragEventArgs e)
+        {
+            ItemsControl dropContainer;
+            UIElement droppedOverElement;
+            int dropIndex;
+            IDragable source;
+            IDropable target;
+
+            if(_droppableType!=null)
+            {
+                if(e.Data.GetDataPresent(_droppableType))
+                {
+                    dropContainer = (ItemsControl)sender;
+                    droppedOverElement = UIHelper.GetUIElement(dropContainer, e.GetPosition(dropContainer));
+                    dropIndex = dropContainer.ItemContainerGenerator.IndexFromContainer(droppedOverElement) + 1;
+
+                    source = (IDragable)e.Data.GetData(_droppableType);
+                    source.Remove(e.Data.GetData(_droppableType));
+
+                    target = (IDropable)AssociatedObject.DataContext;
+                    target.Drop(e.Data.GetData(_droppableType), dropIndex);
+                }
+
+                e.Handled = true;
+            }
+        }
+
+        void AssociatedObject_DragLeave(object sender, DragEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        void AssociatedObject_DragOver(object sender, DragEventArgs e)
+        {
+            if (_droppableType != null)
+            {
+                if(e.Data.GetDataPresent(_droppableType))
+                {
+                    SetDragDropEffects(e);
+                    
+                }
+            }
+            e.Handled = true;
+
+        }
+
+        void AssociatedObject_DragEnter(object sender, DragEventArgs e)
+        {
+            if(_droppableType == null)
+            {
+                if(AssociatedObject.DataContext!=null)
+                {
+                    if(AssociatedObject.DataContext as IDropable != null)
+                    {
+                        _droppableType = ((IDropable)AssociatedObject.DataContext).DataType;
+                    }
+                }
+            }
+
+            e.Handled = true;
+        }
+
+        private void SetDragDropEffects(DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.None;  //default to None
+
+            if (e.Data.GetDataPresent(_droppableType))
+            {
+                e.Effects = DragDropEffects.Move;
+            }
+        }
+
+    }
+}
---
+++ PDFToolbox/Common/UIHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace PDFToolbox.Common
+{
+    internal static class UIHelper
+    {
+        internal static UIElement GetUIElement(ItemsControl container, Point position)
+        {
+            if (container == null) throw new ArgumentNullException("container");
+            if (position == null) throw new ArgumentNullException("position");
+
+            UIElement elementAtPosition = (UIElement)container.InputHitTest(position);
+            object testElement;
+
+            if(elementAtPosition!=null)
+            {
+                while(elementAtPosition!=null)
+                {
+                    testElement = container.ItemContainerGenerator.ItemFromContainer(elementAtPosition);
+                    if(testElement != DependencyProperty.UnsetValue)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        elementAtPosition = (UIElement) VisualTreeHelper.GetParent(elementAtPosition);
+                    }
+                }
+            }
+            return elementAtPosition;
+        }
+
+        internal static bool IsPositionAboveElement(UIElement element, Point relativePosition)
+        {
+            if (element == null) throw new ArgumentNullException("element");
+            if (relativePosition == null) throw new ArgumentNullException("relativePosition");
+
+            return relativePosition.Y < ((FrameworkElement)element).ActualHeight / 2;
+        }
+    }
+}
